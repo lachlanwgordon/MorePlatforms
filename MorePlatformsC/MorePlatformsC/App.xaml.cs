2e6bad56c7409249fd6df76d5ecb45d9de364ce3
Lachlan Gordon <lachlan@cookingthebooks.com.au>
2019-10-10 13:16:40 +1100

    File>New>Tabbed Forms App with UWP. Tested on Android and UWP in Windows

---
+++ MorePlatformsC/MorePlatformsC/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MorePlatformsC.Services;
+using MorePlatformsC.Views;
+
+namespace MorePlatformsC
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
