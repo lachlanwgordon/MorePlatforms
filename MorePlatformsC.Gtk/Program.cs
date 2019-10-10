409fae2f95e6ce2d8e1c16a6e6592ad91d7662a8
Lachlan Gordon <lachlanwgordon@gmail.com>
2019-10-10 14:09:24 +1100

    Created a GTK header project. It compiles on Mac, not yet tested on Windows.

---
+++ MorePlatformsC.Gtk/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Gtk;
+using Xamarin.Forms.Platform.GTK;
+using Xamarin.Forms;
+using Application = Gtk.Application;
+
+namespace MorePlatformsC.Gtk
+{
+    class MainClass
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Application.Init();
+            Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("More Platforms");
+            window.Show();
+
+            Application.Run();
+        }
+    }
+}
